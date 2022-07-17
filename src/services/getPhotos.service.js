622f0ed5c309ecd331c28ee772e2ff40ddd5dd7b
AtulPant2704 <atulpant1999@gmail.com>
2022-07-17 21:54:54 +0530

    refactor: fetch only 10 photos in one network call

--- src/services/getPhotos.service.js
+++ src/services/getPhotos.service.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const getPhotosService = (count) => {
+const getPhotosService = () => {
   const api = "https://api.unsplash.com";
   const accessKey = process.env.REACT_APP_ACCESS_KEY;
-  return axios.get(
-    `${api}/photos/random?client_id=${accessKey}&count=${count}`
-  );
+  return axios.get(`${api}/photos/random?client_id=${accessKey}&count=10`);
 };
 
 export { getPhotosService };
